397bf8e68bf91a045e246e40fcb22ab399157819
Rekia Harrat <rekia.harrat86@gmail.com>
2024-12-17 14:53:11 +0100

    fix: remplacer <details><summary> par le composant Collapse

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,52 +1,30 @@
-import React from 'react';
-import { aboutList } from '../assets/data/aboutData';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
-
-import Footer from '../Components/Footer';
+import React from "react";
+import { aboutList } from "../assets/data/aboutData";
+import Footer from "../Components/Footer";
 import "../Styles/Pages/index.scss";
-import Header from '../Components/Header';
-
+import Header from "../Components/Header";
+import Collapse from "../Components/Collapse";
 
 const About = () => {
-    return (
-        <div className='about'>
-<Header/>
-             
-                 
-                 <img className='montagne' src='/montagne.png' alt='montagne' />
-              
-            
+  return (
+    <div className="about">
+      <Header />
 
-           <section>
-            
-             {aboutList.map((item, index) => (
-         <div key={index} className="about-item">
+      <img className="montagne" src="/montagne.png" alt="montagne" />
 
-          <details>
-            <summary>
-            {item.title}
-            <FontAwesomeIcon icon={faChevronUp} className="chevron-icon" />
-            </summary>
-            {item.content}
-            </details>
-            
-        </div>
-        
-       
-      ))}
+      <section>
+        {aboutList.map((item, index) => (
+          <div key={index} className="about-item">
+            <Collapse title={item.title}>{item.content}</Collapse>
+          </div>
+        ))}
       </section>
-      
-       <footer>
-            <Footer/>
-            </footer> 
-         
 
-        </div>
-        
-
-    );
-    
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default About;
